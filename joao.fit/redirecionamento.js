094c2ac7b1a9d23af9cbf0c55853290e8be7571b
joaodetebayo2 <166837573+joaodetebayo2@users.noreply.github.com>
2024-05-22 19:03:53 +0000

    Atualização 1.1

---
+++ joao.fit/redirecionamento.js
@@ -0,0 +1,4 @@
+// menu.js
+document.getElementById("menuBtn").addEventListener("click", function() {
+    window.location.href = "joao.fit/pages/menu.html"; // Caminho para o arquivo menu.html
+});
