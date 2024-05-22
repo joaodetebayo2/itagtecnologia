e8d1773c18dfd90609ef193bcfe65e7445f53ec4
joaodetebayo2 <166837573+joaodetebayo2@users.noreply.github.com>
2024-05-22 19:29:53 +0000

    Att 1.1 2

--- joao.fit/redirecionamento.js
+++ joao.fit/redirecionamento.js
@@ -1,4 +1,4 @@
 // menu.js
 document.getElementById("menuBtn").addEventListener("click", function() {
-    window.location.href = "joao.fit/pages/menu.html"; // URL completo para o arquivo menu.html
+    window.location.href = "/pages/menu.html"; // Caminho para o arquivo menu.html
 });
