66d22ef3c349969d2b4dc1571173c7b2ca79629c
joaodetebayo2 <166837573+joaodetebayo2@users.noreply.github.com>
2024-05-22 19:07:55 +0000

    Atualização 1.1 nov

--- joao.fit/redirecionamento.js
+++ joao.fit/redirecionamento.js
@@ -1,4 +1,4 @@
 // menu.js
 document.getElementById("menuBtn").addEventListener("click", function() {
-    window.location.href = "joao.fit/pages/menu.html"; // Caminho para o arquivo menu.html
+    window.location.href = "https://joaodetebayo2.github.io/itagtecnologia/menu.html"; // URL completo para o arquivo menu.html
 });
